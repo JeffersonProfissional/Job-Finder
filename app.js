73a022851c2e1274d4a654d268f2b36031e357b1
Jefferson <kio123457@gmail.com>
2021-04-25 14:32:14 -0300

    Criado html e css da página e implementada interações com a mesma usando o bootstrap OBS: Esta parte não foi desenvolvida por mim, ela foi cedida pelo instrutor do curso para os alunos sem experiência no bootstrap, pois o mesmo não explicou detalhadamente o código implementado

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
-
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log('Esta rodando na porta ' + PORT);
 });// Está ouvindo o programa na prota 3000
+
 
 // body parser
 app.use(bodyParser.urlencoded({extended: false})); //usar o corpo do body parser no express - urlenconded: padrão de configuração que o pacote indica (border parser)
